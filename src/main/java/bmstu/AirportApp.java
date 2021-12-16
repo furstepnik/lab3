5a10eb33762002fc086dc77443aba0831b0384e1
furstepnik <furstepnik@yandex.ru>
2021-12-16 23:33:56 +0300

    Scripted auto-commit on change (2021-12-16 23:33:56) by gitwatch.sh

--- src/main/java/bmstu/AirportApp.java
+++ src/main/java/bmstu/AirportApp.java
@@ -4,11 +4,14 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class AirportApp {
-    private static final String FLIGHTS_FILE = ""
+    private static final String FLIGHTS_FILE = "664600583_T_ONTIME_sample.csv";
+    private static final String AIRPORT_FILE = 
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("AirportApp");
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+
 
 
     }
