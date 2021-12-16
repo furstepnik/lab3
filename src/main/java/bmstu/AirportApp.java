4ed664d4acfa5532d633db2213beba0831010760
furstepnik <furstepnik@yandex.ru>
2021-12-17 00:26:28 +0300

    Scripted auto-commit on change (2021-12-17 00:26:28) by gitwatch.sh

--- src/main/java/bmstu/AirportApp.java
+++ src/main/java/bmstu/AirportApp.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 public class AirportApp {
     private static final String FLIGHTS_FILE = "664600583_T_ONTIME_sample.csv";
@@ -12,7 +13,7 @@ public class AirportApp {
     private static final int ID_INDEX = 0;
     private static final int AIRPORT_NAME_INDEX = 1;
     private static final int DEPARTURE_ID = 11;
-    private static final int ARRIVED_ID = 
+    private static final int ARRIVED_ID = 14;
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("AirportApp");
@@ -25,10 +26,17 @@ public class AirportApp {
             String[] column = row.split(SEPARATOR);
             String airportName = column[AIRPORT_NAME_INDEX];
             String airportID = column[ID_INDEX];
+            return new Tuple2<>(airportID, airportName);
         });
 
         JavaPairRDD<Tuple2<String. String>, Flight> flightParse = flightsInput.mapToPair(row -> {
             String[] column = row.split(SEPARATOR);
+            String departureID = column[DEPARTURE_ID];
+            String arrivedID = column[ARRIVED_ID];
+            int cancelled;
+            double delay;
+
+
 
         })
 
