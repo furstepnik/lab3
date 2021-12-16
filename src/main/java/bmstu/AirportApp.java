75fc28085771e7a70841f774dccd16d22c07d80b
furstepnik <furstepnik@yandex.ru>
2021-12-16 23:56:15 +0300

    Scripted auto-commit on change (2021-12-16 23:56:15) by gitwatch.sh

--- src/main/java/bmstu/AirportApp.java
+++ src/main/java/bmstu/AirportApp.java
@@ -8,6 +8,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class AirportApp {
     private static final String FLIGHTS_FILE = "664600583_T_ONTIME_sample.csv";
     private static final String AIRPORT_FILE = "L_AIRPORT_ID.csv";
+    private static final String SEPARATOR = ",";
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("AirportApp");
@@ -17,6 +18,7 @@ public class AirportApp {
         JavaRDD<String> airportInput = sc.textFile(AIRPORT_FILE);
 
         JavaPairRDD<String, String> airportsMap = airportInput.mapToPair(row -> {
+            String[] column = row.split(SEPARATOR);
             
         })
 
