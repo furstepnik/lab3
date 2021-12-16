69b4b3868b623ee5ef3acb8a7e54247af83a96f3
furstepnik <furstepnik@yandex.ru>
2021-12-16 23:30:13 +0300

    Scripted auto-commit on change (2021-12-16 23:30:13) by gitwatch.sh

--- src/main/java/bmstu/AirportApp.java
+++ src/main/java/bmstu/AirportApp.java
@@ -8,6 +8,7 @@ public class AirportApp {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("AirportApp");
         JavaSparkContext sc = new JavaSparkContext(conf);
+        
 
     }
 }
